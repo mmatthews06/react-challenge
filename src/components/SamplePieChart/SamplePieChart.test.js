f5b2af107e47d0ea3fe5a9e82882c915e6638ab1
Michael Matthews <mmatthews06@gmail.com>
2019-08-16 02:34:42 -0400

    quick fix for some linting issues and add one more PieChart test

--- src/components/SamplePieChart/SamplePieChart.test.js
+++ src/components/SamplePieChart/SamplePieChart.test.js
@@ -5,7 +5,11 @@ import { shallow } from 'enzyme';
 import SamplePieChart from './SamplePieChart';
 
 describe('SamplePieChart', () => {
+  it('should render an empty message', () => {
+    expect(shallow(<SamplePieChart active={0} queued={0} onHold={0} />)).toMatchSnapshot();
+  });
+
   it('should match a snapshot', () => {
-    expect(shallow(<SamplePieChart />)).toMatchSnapshot();
+    expect(shallow(<SamplePieChart active={20} queued={10} onHold={3} />)).toMatchSnapshot();
   });
 });
