d20c6fac1e57bc1ecb871db79d7bfc1d66275dff
Michael Matthews <mmatthews06@gmail.com>
2019-08-15 23:17:13 -0400

    initial app setup with top AppBar

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,11 @@
-/* globals document, it */
+/* global describe, expect, it */
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
+
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('App', () => {
+  it('should match a snapshot', () => {
+    expect(shallow(<App />)).toMatchSnapshot();
+  });
 });
